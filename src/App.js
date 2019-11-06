3d84747c58f6b72b0b0e824544e544a14f415f39
namecode12 <50198120+namecode12@users.noreply.github.com>
2019-11-06 17:40:39 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return(
+    <div className="root">
+      <header>head</header>
+      <nav>nav</nav>
+      <div className="daemori">
+        <section>main</section>
+        <aside>aside</aside>
+      </div>
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
