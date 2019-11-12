3ea56ceb961dd06e18c58eac26cdac7321acfa76
namecode12 <50198120+namecode12@users.noreply.github.com>
2019-11-12 19:02:40 +0900

    homework

--- react1107_homework/src/App.jsx
+++ react1107_homework/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import "./App.css"
 import styled from "styled-components";
 import TodoItem from './TodoItem';
-
+import momo from "./check.svg";
 const Div = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  margin-left: 150px;
-  margin-right: 150px;
+  width: 100vw;
+  height: 100vh;
+  background-color: pink;
 `;
 class App extends React.Component {
   constructor(props) {
@@ -35,12 +36,16 @@ class App extends React.Component {
     return (
       <>
         <Div>
+        <div style={{ display:"flex", justifyContent:"center",flexDirection:"row"}} id="fuckfont">TodoList</div>
+          <div style={{ display:"flex", justifyContent:"center",flexDirection:"row"}}>
+          
           <input
             type="text"
             onChange={this.onChange}
             style={({ border: "1px solid black" }, { height: "20px" })}
           />
-          <button onClick={this.onClickAdd}>추가</button>
+          <button onClick={this.onClickAdd} style={({backgroundColor: "pink"},{border:"none"})}><img src={momo} alt="" /></button>
+          </div>
 
           {this.state.items.map((value, idx) => (
             <TodoItem key={idx} value={value} />
